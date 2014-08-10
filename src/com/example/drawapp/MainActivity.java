797c1cdc8249cea90cdb7e5d4ef79ffc5e1b5552
yamadatakumi <takumi.yamada.0419@gmail.com>
2014-08-10 17:30:57 +0900

    円の座標表示

--- src/com/example/drawapp/MainActivity.java
+++ src/com/example/drawapp/MainActivity.java
@@ -1,19 +1,38 @@
 package com.example.drawapp;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
 
+	private Handler handler=new Handler();
+	private View view;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		View view=new SampleView(this);
+		 view=new SampleView(this);
 		setContentView(view);
+		
+		Timer timer=new Timer(false);
+		//匿名クラス（無名クラス）：端折っている
+		timer.schedule(new TimerTask(){
+			public void run(){
+				handler.post(new Runnable(){
+					public void run(){
+						view.invalidate();
+					}
+				});
+			}
+		},0,30);//30msec毎に、viewを再描画させる
+		
 	}
 
 	
--- src/com/example/drawapp/SampleView.java
+++ src/com/example/drawapp/SampleView.java
@@ -1,6 +1,9 @@
 package com.example.drawapp;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,6 +17,11 @@ public class SampleView extends View {
 	private int color =Color.RED;
 	private int bx=100;
 	private int by=100;
+	private int dx=2;
+	private int dy=2;
+	private static int margin=20;
+	private Bitmap item;
+	
 	/**
 	 * @param args
 	 */
@@ -22,11 +30,29 @@ public class SampleView extends View {
 public SampleView(Context context){
 	super(context);
 	setBackgroundColor(Color.WHITE);
+	Resources res=context.getResources();
+	item=BitmapFactory.decodeResource(res, R.drawable.ic_launcher);
+	
 }
 @Override
 public void onDraw(Canvas canvas){
 	paint.setColor(color);
-	canvas.drawCircle(100,100,5,paint);
+	canvas.drawCircle(bx,by,20,paint);
+	//右端に来たら反転
+	if(bx<0+margin){
+		dx=2;
+	}//右端
+	if(bx>getWidth()-margin){
+		dx=-2;
+	}//上端
+	if(by<0+margin){
+		dy=2;
+	}//下端
+	if(by>getHeight()-margin){
+		dy=-2;
+	}
+	bx=bx+dx;
+	by=by+dy;
 }
 @Override
 public boolean onTouchEvent(MotionEvent event){
